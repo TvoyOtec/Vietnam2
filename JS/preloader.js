ee552da0d6cf0c97208d46bea6df0221493398ff
TvoyOtec <44757072+TvoyOtec@users.noreply.github.com>
2022-08-29 13:24:44 +0300

    1.12 hotfix

--- JS/preloader.js
+++ JS/preloader.js
@@ -31,16 +31,3 @@ window.onload = setTimeout(function loaderCloseAnim() {
     }, 500);
 }, 500);
 
-//date
-function NewDate(){
-    let FooterDate = document.querySelector('#Date') 
-    let NowDate = new Date;
-    let NowTime = {
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric'
-    };
-    FooterDate.innerHTML = NowDate.toLocaleString("ru", NowTime);
-};
-NewDate();
-setInterval("NewDate()", 1000);
