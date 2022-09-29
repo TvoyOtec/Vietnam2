9f67f41cc3761227802e072d8f2704ce051fe1d2
TvoyOtec <44757072+TvoyOtec@users.noreply.github.com>
2022-09-29 16:36:51 +0300

    1.43 image changes

--- JS/Console.js
+++ JS/Console.js
@@ -7,130 +7,118 @@ const console = document.querySelector('.console')
 let Count = 1;
 let darkmode = localStorage.getItem("darkmode")
 
-function DarkmodeEnable(){
+function DarkmodeEnable() {
     localStorage.setItem("darkmode", darkmode)
-    if(darkmode == 1){
+    if (darkmode == 1) {
         console.style.background = "rgb(65, 65, 65)"
         inputConsole.style.color = "white";
-    } 
-    // else if(darkmode || 0 === darkmode.length){
-    //     localStorage.setItem("darkmode", false)
-    // }
-    else if (darkmode == 2){
+    }
+    else if (darkmode == 2) {
         console.style.background = "white";
         inputConsole.style.color = "black";
-    } 
+    }
 
 }
 
-function successCommand(f){
+function successCommand(f) {
     SpanNotifyCommand.innerHTML = f;
     SuccessNotifyCommand.style.opacity = "1";
     setTimeout("SuccessNotifyCommand.style.opacity = '0' ", 2000);
 }
 
-function buttonOn(){
+function buttonOn() {
     ButtonMobile.style.display = 'flex';
 }
 
-function buttonOff(){
+function buttonOff() {
     ButtonMobile.style.display = 'none';
 }
 
-function OutpCommand(name){
+function OutpCommand(name) {
     const OutputConsolep = document.createElement('p')
     Count++;
     OutputConsolep.setAttribute('id', `OutputConsole${Count}`)
     OutputConsolep.innerHTML = `-${name}`;
     OutputConsolep.className = 'outputConsole';
-    if(darkmode == true){
-            OutputConsolep.style.color = 'rgb(250, 71, 71)';
-    } else {
-        OutputConsolep.style.color = 'rgb(126, 25, 25)';
-    }
+    OutputConsolep.style.color = 'rgb(250, 71, 71)';
     OutputConsole.append(OutputConsolep);
     OutputConsole.scrollTo(0, Count * 100)
 }
 
-function YourCommandWriter(){
+function YourCommandWriter() {
     const OutputConsolep = document.createElement('p');
     Count++;
     OutputConsolep.setAttribute('id', `OutputConsole${Count}`);
     OutputConsolep.className = 'outputConsole';
     OutputConsolep.innerHTML = `-${inputConsole.value}`;
-    if(darkmode == true){
-        OutputConsolep.style.color = 'rgb(114, 197, 107)';
-    } else {
-        OutputConsolep.style.color = 'rgb(52, 121, 25)';
-    }
+    OutputConsolep.style.color = 'rgb(114, 197, 107)';
     OutputConsole.append(OutputConsolep);
 }
 
-function AddCoins(many){
+function AddCoins(many) {
     localStorage.setItem("ratetick", many)
     OutputConsole.scrollTo(0, Count * 100)
 }
 
-function AddPayDay(many){
+function AddPayDay(many) {
     localStorage.setItem("yantick", many)
     OutputConsole.scrollTo(0, Count * 100)
 }
 
-function Main(e){
-        YourCommandWriter()
-        let huys = Count;
-        let preCommandNum = huys - 2;
-        let preCommand = document.getElementById(`OutputConsole${preCommandNum}`).textContent;
-        if(inputConsole.value == "changecoins"){
-            OutpCommand("how many?")
-            console.log(Count, preCommand, localStorage)
-        } else if(preCommand == "-changecoins"){
-            let coinSCome = parseInt(inputConsole.value);
-            AddCoins(coinSCome)
-            successCommand(`Social rating = ${coinSCome}`)
-        } else if(inputConsole.value == "changepayday"){
-            OutpCommand("how many?")
-        } else if(preCommand == "-changepayday"){
-            let PaydayCome = parseInt(inputConsole.value);
-            AddPayDay(PaydayCome)
-            successCommand(`Заралата теперь равна ${PaydayCome}`)
-        } else if(inputConsole.value == "buttonon"){
-            buttonOn()
-            successCommand(`Кнопка включена`)
-        } else if(inputConsole.value == "buttonoff"){
-            buttonOff()
-            successCommand(`Кнопка выключена`)
-        } else if(inputConsole.value == "help"){
-            OutpCommand("changepayday");
-            OutpCommand("changecoins");
-            OutpCommand("buttonoff");
-            OutpCommand("buttonon");
-            OutpCommand("darkmode");
-            OutpCommand("lightmode");
-        }else if(inputConsole.value == "darkmode"){
-            darkmode = 1
-            localStorage.setItem("darkmode", 1)
-            DarkmodeEnable()
-            successCommand(`Включена темная тема`)
-        }else if(inputConsole.value == "lightmode"){
-            darkmode = 2
-            localStorage.setItem("darkmode", 2)
-            DarkmodeEnable()
-            successCommand(`Включена светлая тема`)
-        }else {
-            OutpCommand("Unknown command");
- 
+function Main(e) {
+    YourCommandWriter()
+    let huys = Count;
+    let preCommandNum = huys - 2;
+    let preCommand = document.getElementById(`OutputConsole${preCommandNum}`).textContent;
+    if (inputConsole.value == "changecoins") {
+        OutpCommand("how many?")
+        console.log(Count, preCommand, localStorage)
+    } else if (preCommand == "-changecoins") {
+        let coinSCome = parseInt(inputConsole.value);
+        AddCoins(coinSCome)
+        successCommand(`Social rating = ${coinSCome}`)
+    } else if (inputConsole.value == "changepayday") {
+        OutpCommand("how many?")
+    } else if (preCommand == "-changepayday") {
+        let PaydayCome = parseInt(inputConsole.value);
+        AddPayDay(PaydayCome)
+        successCommand(`Заралата теперь равна ${PaydayCome}`)
+    } else if (inputConsole.value == "buttonon") {
+        buttonOn()
+        successCommand(`Кнопка включена`)
+    } else if (inputConsole.value == "buttonoff") {
+        buttonOff()
+        successCommand(`Кнопка выключена`)
+    } else if (inputConsole.value == "help") {
+        OutpCommand("changepayday");
+        OutpCommand("changecoins");
+        OutpCommand("buttonoff");
+        OutpCommand("buttonon");
+        OutpCommand("darkmode");
+        OutpCommand("lightmode");
+    } else if (inputConsole.value == "darkmode") {
+        darkmode = 1
+        localStorage.setItem("darkmode", 1)
+        DarkmodeEnable()
+        successCommand(`Включена темная тема`)
+    } else if (inputConsole.value == "lightmode") {
+        darkmode = 2
+        localStorage.setItem("darkmode", 2)
+        DarkmodeEnable()
+        successCommand(`Включена светлая тема`)
+    } else {
+        OutpCommand("Unknown command");
         inputConsole.value = "";
     };
-    
+
 }
-inputConsole.addEventListener('keyup', function(e){
-    if(e.code == "Enter" || e.keycode == "39" || e.keycode == "13"){
+inputConsole.addEventListener('keyup', function (e) {
+    if (e.code == "Enter" || e.keycode == "39" || e.keycode == "13") {
         Main(e)
         inputConsole.value = "";
     }
 })
-ButtonMobile.addEventListener('click', function(e){
+ButtonMobile.addEventListener('click', function (e) {
     Main(e)
     inputConsole.value = "";
 })
--- JS/Vcoin.js
+++ JS/Vcoin.js
@@ -1,6 +1,5 @@
 let kolvo = 1;
 let yanBuy = 5;
-const oneCost = 1000;
 let socrating = parseInt(localStorage.getItem('ratetick'), 10);
 let yan = parseInt(localStorage.getItem('yantick'), 10);;
 const list = document.querySelector('.bonusList');
@@ -9,87 +8,107 @@ const rateinfo = document.querySelector('.socCredit');
 const yaninfo = document.querySelector('.Yan');
 const SuccessNotify = document.querySelector('.SuccesNotifSmall');
 const FailedNotify = document.querySelector('.LessNotifSmall');
+const oneCost = 1000;
 
 // boosts
-while (kolvo < 21) {
-  socrat = yanBuy * oneCost;
-  let li = document.createElement('a');
-  li.innerHTML = `<li class="li" type="none" id="${kolvo}"><img class="liIMG" src="IMAGE/plus.png" alt="" id="${kolvo}"><p id="${kolvo}" class="YanPLus">${yanBuy} ¥</p> <p id="${kolvo}" class="socMinus">-${socrat} social credit</p></li>`;
-  li.className = 'liA';
-  li.setAttribute("href", "#li");
-  li.setAttribute("id", `${kolvo}`);
-  list.append(li);
-  if (yanBuy == 1) {
-    yanBuy = yanBuy + 4;
-  } else {
-    yanBuy = yanBuy + 5;
-  };
-  kolvo = kolvo + 1;
+while (kolvo < 31) {
+    socrat = yanBuy * oneCost;
+    let li = document.createElement('a');
+    li.innerHTML = `<li class="li" type="none" id="${kolvo}"><img class="liIMG" src="IMAGE/plus.png" alt="" id="${kolvo}"><p id="${kolvo}" class="YanPLus">+${yanBuy} ¥</p> <p id="${kolvo}" class="socMinus">-${socrat} social credit</p></li>`;
+    li.className = 'liA';
+    li.setAttribute("href", "#li");
+    li.setAttribute("id", `${kolvo}`);
+    list.append(li);
+    if (yanBuy == 1) {
+        yanBuy = yanBuy + 4;
+    } else {
+        yanBuy = yanBuy + 5;
+    };
+    kolvo = kolvo + 1;
 };
 
 //ratemonitor, payday and click
 
 function payday() {
-  socrating = socrating + yan;
+    socrating = socrating + yan;
 }
 
 function ratingMonitor() {
-  yaninfo.innerHTML = `${yan}`;
-  rateinfo.innerHTML = `${socrating}`;
-  localStorage.setItem("ratetick", socrating)
-  localStorage.setItem("yantick", yan)
-  if (!socrating || 0 === socrating.length) {
-    socrating = 0;
-  };
-  if (!yan || 0 === yan.length) {
-    yan = 0;
-  }
+    yaninfo.innerHTML = `${yan}`;
+    rateinfo.innerHTML = `${socrating}`;
+    localStorage.setItem("ratetick", socrating)
+    localStorage.setItem("yantick", yan)
+    if (!socrating || 0 === socrating.length) {
+        socrating = 0;
+    };
+    if (!yan || 0 === yan.length) {
+        yan = 0;
+    }
 }
 
 ratingMonitor()
 
 button.addEventListener('click', function plus() {
-  socrating++;
-  console.log(socrating);
-  ratingMonitor();
+    socrating++;
+    console.log(socrating);
+    ratingMonitor();
 });
 
 // notification
 function success() {
-  SuccessNotify.style.opacity = "1";
-  setTimeout("SuccessNotify.style.opacity = '0' ", 2000)
+    SuccessNotify.style.opacity = "1";
+    setTimeout("SuccessNotify.style.opacity = '0' ", 2000)
 }
 function failed() {
-  FailedNotify.style.opacity = "1";
-  setTimeout("FailedNotify.style.opacity = '0' ", 2000)
+    FailedNotify.style.opacity = "1";
+    setTimeout("FailedNotify.style.opacity = '0' ", 2000)
 }
 
 
 // boost add script
 list.addEventListener('click', e => {
-  let ids = parseInt(e.target.id);
-  if (ids == 0 && socrating >= oneCost) {
-    socrating = socrating - oneCost;
-    yan = yan + 1;
-    console.log(ids, yan)
-    success();
-  } else if (ids == 0) {
-    console.log('huy', ids);
-    failed();
-  }
-  if (ids > 0) {
-    socrating = socrating - (ids * 5 * oneCost);
-    yan = yan + ids * 5
-    if (socrating < 0) {
-      socrating = socrating + (ids * 5 * oneCost);
-      yan = yan - ids * 5;
-      nea = true;
-      failed();
-    } else {
-      success();
+    let ids = parseInt(e.target.id);
+    if (ids == 0 && socrating >= oneCost) {
+        socrating = socrating - oneCost;
+        yan = yan + 1;
+        console.log(ids, yan)
+        success();
+    } else if (ids == 0) {
+        console.log('huy', ids);
+        failed();
     }
-  }
+    if (ids > 0) {
+        socrating = socrating - (ids * 5 * oneCost);
+        yan = yan + ids * 5
+        if (socrating < 0) {
+            socrating = socrating + (ids * 5 * oneCost);
+            yan = yan - ids * 5;
+            nea = true;
+            failed();
+        } else {
+            success();
+        }
+    }
 })
+
+// click effect
+// let CoinButton = document.querySelectorAll('.liA')
+// CoinButton.forEach(btn => {
+//     btn.addEventListener('click', function (e) {
+//         let x = e.pageX
+//         let y = e.pageY
+//         let rippleeffect = document.createElement('span')
+//         rippleeffect.className = "ripple"
+//         rippleeffect.style.left = `${x}px`
+//         rippleeffect.style.top = `${y}px`
+//         this.appendChild(rippleeffect)
+
+//         setInterval(() => {
+//             rippleeffect.remove()
+//         }, 900);
+//     })
+// })
+
 
 setInterval("ratingMonitor()", 100);
 setInterval("payday()", 1000);
@@ -115,8 +134,3 @@ setInterval("payday()", 1000);
 //         return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
 //     }
 // };
-
-// if (isMobile.any()) {
-//     document.querySelector('.li')
-// }
-
