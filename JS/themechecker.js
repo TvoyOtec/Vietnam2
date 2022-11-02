4d920be2b05a1aa58e1dfc5b1d911c918482d84f
TvoyOtec <44757072+TvoyOtec@users.noreply.github.com>
2022-11-02 17:14:28 +0300

    1.5.1 dark mode ready hotfix

--- JS/themechecker.js
+++ JS/themechecker.js
@@ -3,13 +3,21 @@ const header1 = document.querySelector('header');
 const header1a = document.querySelector('.hname_a');
 let theme = localStorage.getItem('darkmode');
 
+function HeadersChanger() {
+    body.className = "bodyBlack";
+    header1.classList.add('headerBlack');
+    header1a.classList.add('hname_aBlack');
+    let butupstyle = document.head.appendChild(document.createElement("style"));
+    butupstyle.innerHTML = ".butUp:before,.butUp:after{background: rgb(218, 218, 218);} #headerIMG {filter: invert(80%);-webkit-filter: invert(80%);} .bonusInfo::before {background: white;}";
+
+}
 
 function ThemeChecker1() {
     if (theme == 1) {
-        const footer = document.querySelector('footer');
         body.className = "bodyBlack";
         header1.classList.add('headerBlack');
         header1a.classList.add('hname_aBlack');
+        const footer = document.querySelector('footer');
         footer.classList.add('footerBlack');
         document.querySelector('.slider').style.filter = "brightness(0.7)"
     };
@@ -25,22 +33,14 @@ function ThemeChecker2() {
         document.querySelector('.info').style.color = 'rgb(218, 218, 218)'
         document.querySelector('h3').style.color = 'rgb(218, 218, 218)'
         document.querySelector('.bonusInfo').style.color = 'rgb(218, 218, 218)'
-        let butupstyle = document.head.appendChild(document.createElement("style"));
-        butupstyle.innerHTML = ".butUp:before,.butUp:after{background: rgb(218, 218, 218);} #headerIMG {filter: invert(80%);-webkit-filter: invert(80%);}";
-        body.className = "bodyBlack";
-        header1.classList.add('headerBlack');
-        header1a.classList.add('hname_aBlack');
+        HeadersChanger()
     };
 
 };
 
 function ThemeChecker3() {
     if (theme == 1) {
-        let butupstyle = document.head.appendChild(document.createElement("style"));
-        butupstyle.innerHTML = ".butUp:before,.butUp:after{background: rgb(218, 218, 218);} #headerIMG {filter: invert(80%);-webkit-filter: invert(80%);}";
-        body.className = "bodyBlack";
-        header1.classList.add('headerBlack');
-        header1a.classList.add('hname_aBlack');
+        HeadersChanger()
         console.style.background = "rgb(65, 65, 65)"
         inputConsole.style.color = "white";
     }
@@ -51,11 +51,7 @@ function ThemeChecker4() {
     if (theme == 1) {
         document.querySelector('.calculator').style.boxShadow = 'box-shadow: 0 0 10px rgb(59, 59, 59), inset 0 0 30px rgb(59, 59, 59);'
         document.querySelector('.historyList').className = 'historylistBlack'
-        let butupstyle = document.head.appendChild(document.createElement("style"));
-        butupstyle.innerHTML = ".butUp:before,.butUp:after{background: rgb(218, 218, 218);} #headerIMG {filter: invert(80%);-webkit-filter: invert(80%);}";
-        body.classList.add("bodyBlack")
-        header1.classList.add('headerBlack');
-        header1a.classList.add('hname_aBlack');
+        HeadersChanger()
     };
 
 };
